e213c6ea2ed6a8f4cff9f91b408504992564f028
Mostafa Saied <110390548+Mostafa572001@users.noreply.github.com>
2024-05-10 21:54:55 +0300

    Update client.py

--- client server unlimitted/client.py
+++ client server unlimitted/client.py
@@ -1,11 +1,21 @@
-from socket import *
-s = socket(AF_INET, SOCK_STREAM)
+import socket
 
-host = "127.0.0.1"
-port = 40674
+def start_client(host, port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+    print(f"Connected to server on {host}:{port}")
 
-s.connect((host,port))
+    while True:
+        message = input("Enter message to send (or type 'exit' to quit): ")
+        if message.lower() == "exit":
+            break
 
-print(s.recv(2048))
+        client_socket.sendall(message.encode())  # Send message to server
 
-s.close()
+    client_socket.close()
+
+if __name__ == "__main__":
+    HOST = "127.0.0.1"  # localhost
+    PORT = 12345  # Same port as the server
+
+    start_client(HOST, PORT)
